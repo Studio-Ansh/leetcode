1868f94fcd6682511f5291801459f9a063c3f95b
Ansh Shrivastava <learnersobject@gmail.com>
2026-06-26 18:26:11 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (56.74%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,30 +1,58 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-        vector<int> prefix(n, 0);
-        vector<int> suffix(n, 0);
-        int total =0;
+        int lmax = 0;
+        int rmax = 0;
 
-        prefix[0] = height[0];
-        suffix[n-1]= height[n-1];
+        int l =0;
+        int r =height.size()-1;
+        int ans = 0;
 
-        for(int i =1; i<n; i++) {
-            prefix[i] = max(prefix[i-1],height[i]);
+        while(l <= r) {
+
+            lmax = max(lmax, height[l]);
+            rmax = max(rmax, height[r]);
+
+            if(lmax < rmax){
+                ans+= lmax - height[l];
+                l++;
+            }
+                
+
+            else {
+                ans+= rmax - height[r];
+                r--;
+            }
+            
+
         }
+        return ans;
 
-        for(int i =n-2; i>= 0; i--) {
-            suffix[i] = max(suffix[i+1], height[i]);
-        }
 
-        for(int i=0; i<n; i++) {
-            int leftM= prefix[i];
-            int rightM= suffix[i];
+        // int n = height.size();
+        // vector<int> prefix(n, 0);
+        // vector<int> suffix(n, 0);
+        // int total =0;
 
-            if(height[i] <leftM && height[i] < rightM)
-                total += min(leftM, rightM) - height[i];
+        // prefix[0] = height[0];
+        // suffix[n-1]= height[n-1];
 
-        }
-        return total;
+        // for(int i =1; i<n; i++) {
+        //     prefix[i] = max(prefix[i-1],height[i]);
+        // }
+
+        // for(int i =n-2; i>= 0; i--) {
+        //     suffix[i] = max(suffix[i+1], height[i]);
+        // }
+
+        // for(int i=0; i<n; i++) {
+        //     int leftM= prefix[i];
+        //     int rightM= suffix[i];
+
+        //     if(height[i] <leftM && height[i] < rightM)
+        //         total += min(leftM, rightM) - height[i];
+
+        // }
+        // return total;
     }
 };
